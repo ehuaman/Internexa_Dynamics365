e56cbc900d5cce26bc8b61729ba9323598557f26
Administrador <Administrador@LPT-028>
2018-02-19 09:39:53 -0500

    Se crean clases de factibilidad

--- src/test/java/com/internexa/dynamics/definitions/LoginDynamicsDefinition.java
+++ src/test/java/com/internexa/dynamics/definitions/LoginDynamicsDefinition.java
@@ -1,6 +1,7 @@
 package com.internexa.dynamics.definitions;
 
 
+import com.internexa.dynamics.steps.FactibilidadSteps;
 import com.internexa.dynamics.steps.LoginDynamicsSteps;
 import com.internexa.dynamics.steps.MenuOportunidadSteps;
 import com.internexa.dynamics.steps.OportunidadesAbiertasSteps;
@@ -15,6 +16,7 @@ import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
 
 public class LoginDynamicsDefinition {
+	
 	
 	@Steps
 	LoginDynamicsSteps loginDynamicsSteps;
@@ -33,6 +35,9 @@ public class LoginDynamicsDefinition {
 	
 	@Steps
 	ProductoEditarSteps productoEditarSteps;
+	
+	@Steps
+	FactibilidadSteps factibilidadSteps;
 	
 	@Given("^me logueo al aplicativo Dynamics como \"([^\"]*)\" con pass \"([^\"]*)\"$")
 	public void me_logueo_al_aplicativo_Dynamics_como_con_pass(String strUsuario, String strPass) throws Throwable {
@@ -62,10 +67,37 @@ public class LoginDynamicsDefinition {
 	public void se_ingresa_a_la_oportunidad_y_se_elige_la_opcion_y_se_ingresa_el_producto(String productoRegistrar) throws Throwable {
 		oportunidadSteps.oportunidad_producto(productoRegistrar);
 	}	
+	
+	@Then("^se ingresa a la pagina de propiedades del producto se elige las instancias requeridas y que posean campo de valor propiedad vacio se valida el tipo de dato y se ingresa para agregar valor propiedad capacidad \"([^\"]*)\" ciudad a \"([^\"]*)\" ciudad b \"([^\"]*)\" dirección \"([^\"]*)\"$")
+	public void se_ingresa_a_la_pagina_de_propiedades_del_producto_se_elige_las_instancias_requeridas_y_que_posean_campo_de_valor_propiedad_vacio_se_valida_el_tipo_de_dato_y_se_ingresa_para_agregar_valor_propiedad_capacidad_ciudad_a_ciudad_b_dirección(String propiedadCapacidad, String propiedadCiudadA, String propiedadCiudadB, String propiedadDireccion) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    productoEditarSteps.editar_producto_oportunidad (propiedadCapacidad, propiedadCiudadA,  propiedadCiudadB, propiedadDireccion);
+	}
+	@Then("^se ingresa la factibilidad$")
+	public void se_ingresa_la_factibilidad() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+	factibilidadSteps.ingresaFactibilidad();
+}
 
-	@Then("^se ingresa a la pagina de propiedades del producto se elige las instancias requeridas y que posean campo de valor propiedad vacio se valida el tipo de dato y se ingresa para agregar valor propiedad$")
-	public void se_ingresa_a_la_pagina_de_propiedades_del_producto() throws Throwable {
-		productoEditarSteps.editar_producto_oportunidad();
-	}	
+@Then("^se detallan datos de factibilidad$")
+	public void se_detallan_datos_de_factibilidad() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+	factibilidadSteps.llenarIPS("Sebastian Orozco");
+	factibilidadSteps.llenarObservacion("Pruebas factibilidad 12233");
+}
+
+/*@When("^selecciono oportunidad que requiere aprobación$")
+public void selecciono_oportunidad_que_requiere_aprobación() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^Se aprueba Factibilidad$")
+public void se_aprueba_Factibilidad() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+	
+
+	}	*/
 	
 }
--- src/test/resources/features/Internexa/Dynamics/LoginDynamics.feature
+++ src/test/resources/features/Internexa/Dynamics/LoginDynamics.feature
@@ -7,6 +7,14 @@ Scenario: Creacion de Oportunidades
 Given me logueo al aplicativo Dynamics como "jbedoya@internexa.com" con pass "Feb2018*"
 And elijo Ventas luego Oportunidades
 When selecciono NUEVO
-Then se presenta la pantalla de Nuevo Oportunidad y se ingresa la cuenta "Avantel S.A." el nombre "PRUEBA Auto1.4" el tipo de venta "Nuevo cliente" fecha estimada de venta "16/02/2018" probabilidad de exito "60%" fecha estimada de ingreso "17/02/2018" tipo de oportunidad "Proyecto" contrato a meses "9" y se guarda la oportunidad
-Then se ingresa a la oportunidad y se elige la opcion y  se ingresa el producto "40"
-Then se ingresa a la pagina de propiedades del producto se elige las instancias requeridas y que posean campo de valor propiedad vacio se valida el tipo de dato y se ingresa para agregar valor propiedad
+Then se presenta la pantalla de Nuevo Oportunidad y se ingresa la cuenta "Avantel S.A." el nombre "PRUEBA Auto1.4" el tipo de venta "Nuevo cliente" fecha estimada de venta "20/02/2018" probabilidad de exito "40%" fecha estimada de ingreso "21/02/2018" tipo de oportunidad "Proyecto" contrato a meses "9" y se guarda la oportunidad
+And se ingresa a la oportunidad y se elige la opcion y  se ingresa el producto "40" 
+And se ingresa a la pagina de propiedades del producto se elige las instancias requeridas y que posean campo de valor propiedad vacio se valida el tipo de dato y se ingresa para agregar valor propiedad capacidad "120" ciudad a "LIMA" ciudad b "bogotá" dirección "Av Republica de Pananma 1123"
+And se ingresa la factibilidad 
+And se detallan datos de factibilidad
+
+@ApruebaFactibilidad
+Scenario: Aprueba Factibilidad
+Given me logueo al aplicativo Dynamics como "gbustamante@internexa.com" con pass "Enero2018*"
+When selecciono oportunidad que requiere aprobación
+Then Se aprueba Factibilidad
